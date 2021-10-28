2fc8ad7caf748ea18ed699e10a245138d68f95d1
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2021-10-28 22:26:19 +0400

    Update index.js

--- src/components/TaskPage/index.js
+++ src/components/TaskPage/index.js
@@ -1,35 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import API from '../../utils/api';
-import Task from '../Task';
+import React, { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import API from "../../utils/api"
+import StaticTask from "../StaticTask"
 
-export default function TaskPage(){
-
-	const { id } = useParams();
+export default function TaskPage() {
+	const { id } = useParams()
 
 	const [task, setTask] = useState({
-		question: '',
-		code: '',
-		annotation: '',
-	});
+		question: "",
+		code: "",
+		annotation: "",
+	})
 
 	useEffect(() => {
-		API.tasks.findOneById(+id)
-			.then(data => {
-				console.log(data);
-				return data;
+		API.tasks
+			.findOneById(+id)
+			.then((data) => {
+				console.log(data)
+				return data
 			})
-			.then(data => setTask(data))
+			.then((data) => setTask(data))
 			.then(() => window.dispatchEvent(new CustomEvent("taskReady")))
-	}, [id]);
+	}, [id])
 
 	return (
 		<div>
-			<Task 
-				task={task} 
-				setTask={setTask}
-				editable={false}
-			/>
+			<StaticTask task={task} />
 		</div>
-	);
+	)
 }
