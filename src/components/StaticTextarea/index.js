d23fc400959ef967a1a9f3acb53402beeade4c01
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2021-10-31 10:11:43 +0400

    Фикс невидимых строк в вопросе задания

--- src/components/StaticTextarea/index.js
+++ src/components/StaticTextarea/index.js
@@ -1,13 +1,23 @@
-import React from "react"
+import React, {useEffect} from "react"
 
 import "./style.css"
 
 export default function StaticTextarea({ text, className }) {
 	const lines = text.replace(/\t/g, " ").split(/\n/)
 	const rows = lines.length
-	const cols = Math.max(...lines.map((line) => line.length)) + 1
+	const cols = Math.max(...lines.map((line) => line.length)) + 7
+
+	const textareaRef = React.createRef();
 
 	className = className ? className + " textarea-static" : "textarea-static"
+
+	useEffect(() => {
+		const textarea = textareaRef.current;
+		textarea.style.height = "auto";
+		textarea.style.height = textarea.scrollHeight + "px";
+
+		
+	}, [text, textareaRef])
 
 	return (
 		<textarea
@@ -15,6 +25,7 @@ export default function StaticTextarea({ text, className }) {
 			defaultValue={lines.join("\n")}
 			rows={rows}
 			cols={cols}
+			ref={textareaRef}
 			disabled
 		/>
 	)
