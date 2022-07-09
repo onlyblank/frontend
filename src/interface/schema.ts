fc533d500c5420db4b21b6fcb4667db3207e02c7
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-07-09 17:09:43 +0300

    Add service types

---
+++ src/interface/schema.ts
@@ -0,0 +1,19 @@
+export interface Identifiable {
+	id: number
+}
+
+export interface CreateUpdate {
+	createdAt: string
+	updatedAt: string
+}
+
+export interface Publishable {
+	publishedAt: string
+}
+
+export type Resource<T> = {
+	attributes: T
+} & Identifiable
+
+export type Component<Name extends string, Data> = Data &
+	Identifiable & { __component: Name }
