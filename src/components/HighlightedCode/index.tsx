ca5431084fe78d8bd06d8416541aaac9f99b6db2
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-07-11 12:56:27 +0300

    Update index.tsx

--- src/components/HighlightedCode/index.tsx
+++ src/components/HighlightedCode/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 import CodeMirror from "codemirror"
 import "codemirror/lib/codemirror.css"
 import "codemirror/mode/clike/clike"
